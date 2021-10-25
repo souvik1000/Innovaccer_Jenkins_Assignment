f625d90f980dc6832f92d66dbcebe2caa0cb3e8b
RocksRiko <60292800+souvik1000@users.noreply.github.com>
2021-10-25 23:09:26 +0530

    Update file_for_jenkins.py

--- file_for_jenkins.py
+++ file_for_jenkins.py
@@ -1,14 +1,29 @@
-from DevelopedCode import Calculator
+#from DevelopedCode import Calculator
 import unittest
+  
+class Calculator:
+    def __init__(self, number1, number2):
+        self.number1 = number1
+        self.number2 = number2
 
+    def addition(self):
+        return (self.number1 + self.number2)
+    
+    def substraction(self):
+        return (self.number1 - self.number2)
 
+    def multiply(self):
+        return (self.number1 * self.number2)
+    
+    def divition(self):
+        return (self.number1 // self.number2)
 
 class testingUser(unittest.TestCase):
     def setUp(self):
-        self.add_check = Calculator.Calculator(10, 20)
-        self.sub_check = Calculator.Calculator(30, 20)
-        self.mult_check = Calculator.Calculator(7, 8)
-        self.div_check = Calculator.Calculator(70, 7)
+        self.add_check = Calculator(10, 20)
+        self.sub_check = Calculator(30, 20)
+        self.mult_check = Calculator(7, 8)
+        self.div_check = Calculator(70, 7)
 
     def test_addition(self):
         self.assertEqual(30, self.add_check.addition())
